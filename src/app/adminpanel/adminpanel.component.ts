522fb1ef3e0282060bb1008949cb0c59cbdb1c8b
Esteban Montoya Herrera <estebanmontoyaherrera7@gmail.com>
2024-06-27 17:10:11 -0500

    Primer commit: Agrega archivos iniciales del proyecto Angular

---
+++ src/app/adminpanel/adminpanel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-adminpanel',
+  standalone: true,
+  imports: [],
+  templateUrl: './adminpanel.component.html',
+  styleUrl: './adminpanel.component.css'
+})
+export class AdminpanelComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AdminpanelComponent } from './adminpanel/adminpanel.component'; // Asegúrate de ajustar la ruta de importación según tu estructura de archivos
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'admin', component: AdminpanelComponent },
+  // Puedes añadir más rutas según sea necesario
+];
+
