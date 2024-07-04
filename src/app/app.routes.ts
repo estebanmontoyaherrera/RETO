cb3aa30fdce395ff40745843ff99481b2acc84b4
Esteban Montoya Herrera <estebanmontoyaherrera7@gmail.com>
2024-07-03 20:35:03 -0500

    Se crea carpeta views y se mueven todas vista para esa carpeta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { AdminpanelComponent } from './adminpanel/adminpanel.component'; // Asegúrate de ajustar la ruta de importación según tu estructura de archivos
-import { ChartLineComponent } from './chart-line/chart-line.component';
+import { HomeComponent } from './views/home/home.component';
+import { LoginComponent } from './views/login/login.component';
+import { RegisterComponent } from './views/register/register.component';
+import { AdminpanelComponent } from './views/adminpanel/adminpanel.component'; // Asegúrate de ajustar la ruta de importación según tu estructura de archivos
+import { ChartLineComponent } from './views/chart-line/chart-line.component';
 
 
 export const routes: Routes = [
