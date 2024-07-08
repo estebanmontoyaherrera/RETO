eb18ec84936a3166a041da98473b36228d6c98f6
Victor Manuel Zapata <victor.zapata.emt01@gmail.com>
2024-07-07 19:25:34 -0500

    Se descargan cambios

---
+++ src/app/component-name/component-name.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-component-name',
+  standalone: true,
+  imports: [],
+  templateUrl: './component-name.component.html',
+  styleUrl: './component-name.component.css'
+})
+export class ComponentNameComponent {
+
+}
