d1ae50b938bfcf3c9000f48880960e22b1b6c1bc
Esteban Montoya Herrera <estebanmontoyaherrera7@gmail.com>
2024-07-23 23:29:56 -0500

    Mejora a Registro y Login

--- src/app/views/about/about.component.ts
+++ src/app/views/about/about.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../components/navbar/navbar.component";
+import { FooterComponent } from "../../components/footer/footer.component";
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, FooterComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
