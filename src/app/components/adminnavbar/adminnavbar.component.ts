e25c88563f4d3b5b2ac9907f726a14636f8186f7
Esteban Montoya Herrera <estebanmontoyaherrera7@gmail.com>
2024-07-23 22:02:00 -0500

    Se actualiza adminnavar con cerrar sesión y mostrar correo en el perfil

--- src/app/components/adminnavbar/adminnavbar.component.ts
+++ src/app/components/adminnavbar/adminnavbar.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 @Component({
   selector: 'app-adminnavbar',
   standalone: true,
@@ -7,6 +7,17 @@ import { Component } from '@angular/core';
   templateUrl: './adminnavbar.component.html',
   styleUrl: './adminnavbar.component.css'
 })
-export class AdminnavbarComponent {
+export class AdminnavbarComponent implements OnInit {
+  userEmail: string | null = null;
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit(): void {
+    this.userEmail = localStorage.getItem('userEmail'); // Recupera el correo electrónico del usuario
+  }
+
+  handleLogout(): void {
+    this.authService.logout(); // Llama al método logout del servicio de autenticación
+  }
 
 }
