a4e2c3bcaab41a0c084bdf015509b0658c767208
Esteban Montoya Herrera <estebanmontoyaherrera7@gmail.com>
2024-07-25 19:02:23 -0500

    Se agrega tabla de registro en en admin panel con datatable

--- src/app/views/adminpages/records/records.component.ts
+++ src/app/views/adminpages/records/records.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { AdminsidebarComponent } from "../../../components/adminsidebar/adminsidebar.component";
 import { AdminnavbarComponent } from "../../../components/adminnavbar/adminnavbar.component";
 import { AdminfooterComponent } from "../../../components/adminfooter/adminfooter.component";
+import $ from 'jquery';
+import 'datatables.net';
+import 'datatables.net-bs5';
 
 @Component({
   selector: 'app-records',
@@ -10,6 +13,13 @@ import { AdminfooterComponent } from "../../../components/adminfooter/adminfoote
   templateUrl: './records.component.html',
   styleUrl: './records.component.css'
 })
-export class RecordsComponent {
+export class RecordsComponent implements AfterViewInit {
 
+  ngAfterViewInit(): void {
+    // Asegúrate de que el elemento con id 'example' está presente en el DOM
+    ($('#example') as any).DataTable({
+      rowReorder: true
+      // Configuración opcional aquí
+    });
+  }
 }
