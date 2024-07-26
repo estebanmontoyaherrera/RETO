aceacc2d8998bdaf87e3146184801c6c2281da1c
Esteban Montoya Herrera <estebanmontoyaherrera7@gmail.com>
2024-07-25 20:26:38 -0500

    Mejora de datatable con los iconos y estilos

--- src/app/views/adminpages/records/records.component.ts
+++ src/app/views/adminpages/records/records.component.ts
@@ -18,8 +18,24 @@ export class RecordsComponent implements AfterViewInit {
   ngAfterViewInit(): void {
     // Asegúrate de que el elemento con id 'example' está presente en el DOM
     ($('#example') as any).DataTable({
-      rowReorder: true
-      // Configuración opcional aquí
+
+      language: {
+        "decimal": "",
+        "emptyTable": "No hay información",
+        "info": "Mostrando _START_ a _END_ de _TOTAL_ Entradas",
+        "infoEmpty": "Mostrando 0 to 0 of 0 Entradas",
+        "infoFiltered": "(Filtrado de _MAX_ total entradas)",
+        "infoPostFix": "",
+        "thousands": ",",
+        "lengthMenu": "Mostrar _MENU_ Entradas",
+        "loadingRecords": "Cargando...",
+        "processing": "Procesando...",
+        "search": "Buscar:",
+        "zeroRecords": "Sin resultados encontrados",       
+    },
+
+    
+    
     });
   }
 }
